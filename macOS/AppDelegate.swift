c34c43ec70af421065a439edf6d72fe179fdf410
John Saba <john.thomas.saba@gmail.com>
2017-04-29 15:25:53 -0500

    initial commit

---
+++ macOS/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  CatShipUniv
+//
+//  Created by John Saba on 4/29/17.
+//  Copyright © 2017 John Saba. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
